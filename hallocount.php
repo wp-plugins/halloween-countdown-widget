87ef38df6199d9b497bd68df445c55b2f26a92ed
monkeymays <monkeymays@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-06 03:53:53 +0000

    git-svn-id: https://plugins.svn.wordpress.org/halloween-countdown-widget/trunk@608788 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ hallocount.php
@@ -0,0 +1,89 @@
+<?php /*
+Plugin Name: Halloween Countdown Widget
+Plugin URI: http://christmaswebmaster.com/vampire-halloween-countdown-wordpress-plugin
+Description: Displays a cute Vampire countdown to Halloween in your sidebar.
+Author: Monica Mays
+Author URI: http://christmaswebmaster.com/
+Version: 1.0
+
+
+Halloween Countdown Widget is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or 
+any later version.
+
+Halloween Countdown Widget is distributed in the hope that it will be useful, 
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with the Halloween Countdown Widget. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+
+ 
+
+class hallocount extends WP_Widget
+{
+  function hallocount()
+  {
+    $widget_ops = array('classname' => 'hallocount', 'description' => 'Displays a cute Halloween countdown' );
+    $this->WP_Widget('hallocount', 'Halloween Countdown', $widget_ops);
+  }
+ 
+  function form($instance)
+  {
+    $instance = wp_parse_args( (array) $instance, array( 'title' => '' ) );
+    $title = $instance['title'];
+?>
+  <p><label for="<?php echo $this->get_field_id('title'); ?>">Title: <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo attribute_escape($title); ?>" /></label></p>
+<?php
+  }
+ 
+  function update($new_instance, $old_instance)
+  {
+    $instance = $old_instance;
+    $instance['title'] = $new_instance['title'];
+    return $instance;
+  }
+ 
+  function widget($args, $instance)
+  {
+    extract($args, EXTR_SKIP);
+ 
+    echo $before_widget;
+    $title = empty($instance['title']) ? ' ' : apply_filters('widget_title', $instance['title']);
+ 
+    if (!empty($title))
+     echo $before_title . $title . $after_title;;
+     echo "<div style=\"background-image: url(".plugins_url('img/vampy.png', __FILE__)."); margin: 0 auto;
+     height:162px; 
+     width:180px; 
+     background-repeat: no-repeat;
+     background-size: 180px 162px;
+     text-align:center;\"><div style=\"font-weight:normal;
+     font-size:15px; 
+     padding-right: 0px; 
+     padding-top:88px; 
+     text-align:center;
+     font-family: comic sans ms;
+     color: #FFFF00;\"><script language=\"javascript\" type=\"text/javascript\">
+        today = new Date();
+        BigDay = new Date(\"October 31, 2012\")
+        msPerDay = 24 * 60 * 60 * 1000;
+        timeLeft = (BigDay.getTime() - today.getTime());
+        e_daysLeft = timeLeft / msPerDay;
+        daysLeft = Math.floor(e_daysLeft);
+        e_hrsLeft = (e_daysLeft - daysLeft) * 24;
+        hrsLeft = Math.floor(e_hrsLeft);
+        minsLeft = Math.floor((e_hrsLeft - hrsLeft) * 60);
+        document.write(\"\" + daysLeft + \" days<BR>til Halloween!\");
+    </script></div></div>";
+    echo $after_widget;
+  }
+ 
+}
+
+add_action( 'widgets_init', create_function('', 'return register_widget("hallocount");') );?>
---
+++ readme.txt
@@ -0,0 +1,61 @@
+=== Halloween Countdown Widget ===
+
+Contributors: monkeymays
+Donate Link: http://christmaswebmaster.com/vampire-halloween-countdown-wordpress-plugin
+Tags: halloween countdown, halloween, holiday, vampire, widget-only
+Requires at least: 3.0
+Tested up to: 3.4.1
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Displays a cute vampire Halloween countdown in your sidebar.
+
+
+
+== Description ==
+
+The Halloween Countdown Widget displays a cute vampire countdown to Halloween in your sidebar. 
+
+
+
+== Installation ==
+
+
+
+1. Upload `hallocount.php` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Go to your widgets screen, and drag the Halloween Countdown widget to your sidebar.
+4. Let the countdown to Halloween begin!
+
+
+
+== Frequently Asked Questions ==
+
+
+
+= Does the bat have a name? =
+
+Of course he does! We call him Vampy.
+
+
+
+
+== Screenshots ==
+
+
+1. Upload plugin and install.
+2. On the plugins panel activate the Halloween Countdown Widget.
+3. In the widgets menu drag the Halloween Countdown Widget to your sidebar.
+4. The Halloween Countdown Widget displays a cute Vampire countdown to Halloween in your sidebar!
+
+
+
+== Changelog ==
+
+Version 1.0 Released 10/05/2012
+
+
+== Upgrade Notice ==
+
+Version 1.0 Released 10/05/2012
